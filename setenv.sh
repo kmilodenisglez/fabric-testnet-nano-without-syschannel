d1dc0938064c0dd5d63de469e033504c34b681bd
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-09-12 19:15:26 -0400

    added ORDERER_ADDRESS and ORDERER_TLS_CA environment var

--- setenv.sh
+++ setenv.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
-# look for binaries in local dev environment /build/bin directory and then in local samples /bin directory
+# change by chaincode name
+export CC_NAME=basic
+# remember to modify CHAINCODE_ID by the one returned by the peer install
+export CHAINCODE_ID=_chaincode_id_here_
+export CHAINCODE_SERVER_ADDRESS=127.0.0.1:9999
+
+# look for binaries in local samples /bin directory
 export PATH="${PWD}"/../bin:"$PATH"
 export FABRIC_CFG_PATH="${PWD}"/../config
 
-# cambiar por que el nombre de su chaincode a consultar
-export CC_NAME=basic
 export CHANNEL_NAME=mychannel
 export FABRIC_LOGGING_SPEC=INFO
 export CORE_PEER_TLS_ENABLED=true
@@ -13,3 +17,7 @@ export CORE_PEER_TLS_ROOTCERT_FILE="${PWD}"/crypto-config/peerOrganizations/org1
 export CORE_PEER_ADDRESS=127.0.0.1:7051
 export CORE_PEER_LOCALMSPID=Org1MSP
 export CORE_PEER_MSPCONFIGPATH="${PWD}"/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+
+# to query the chaincode (peer chaincode invoke and peer chaincode query)
+export ORDERER_ADDRESS=127.0.0.1:6050
+export ORDERER_TLS_CA=${CRYPTO_PATH}/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt
